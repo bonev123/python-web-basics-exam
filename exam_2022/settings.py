14ff7d7c8a6295a14d7ece7d10024e6c930181ac
bonev1234 <bonev123_1996@abv.bg>
2022-05-13 16:02:13 +0300

    extracted secret key from settings.py file

--- exam_2022/settings.py
+++ exam_2022/settings.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#l7vv6*$1^z2g$7i0nd7nfp&e08yr&-n#lg%+9&62r71)rzxw6'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
